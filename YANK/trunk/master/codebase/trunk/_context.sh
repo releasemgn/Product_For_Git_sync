ba67410012d2f461744a438f387230003da2ca89
urmuser <erik.dubois@gmail.com>
2019-04-01 15:29:13 +0300

    1

---
+++ YANK/trunk/master/codebase/trunk/_context.sh
@@ -0,0 +1,3 @@
+export C_URM_PRODUCT=Regression_test
+export C_URM_SERVER=urm.space:8001
+export C_URM_VERSIONMODE=trunk
---
+++ YANK/trunk/master/deploy/Regression_env/_context.sh
@@ -0,0 +1,4 @@
+export C_URM_PRODUCT=Regression_test
+export C_URM_SERVER=urm.space:8001
+export C_URM_ENV=Regression_env
+export C_URM_SG=firstvds
